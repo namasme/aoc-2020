d5bf0968ac36efeed1e5ebdc7af07e994566d325
Nacho Mas <ignaciomasmesa@gmail.com>
2020-12-14 21:34:15 +0100

    Includes a Bézout coefficients calculator utility function
    * Also adapted from last year. Will come in handy to implement CRT solver.

--- src/aoc/utils/__init__.py
+++ src/aoc/utils/__init__.py
@@ -1,6 +1,25 @@
 from pathlib import Path
 
 
+def bezout(a, b):
+    '''
+    The return value of this function verifies that
+             r * a + s * b = gcd = gcd(a, b)
+    '''
+    prev_m, m = 1, 0
+    prev_n, n = 0, 1
+
+    while b != 0:
+        q, r = divmod(a, b)
+
+        a, b = b, r
+        prev_m, m = m, prev_m - q * m
+        prev_n, n = n, prev_n - q * n
+
+    gcd, r, s = a, prev_m, prev_n
+    return (gcd, r, s)
+
+
 def local_path(base, filename):
     return Path(base).resolve().parent.joinpath(filename)
 
